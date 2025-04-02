dda7bb428a7b2cbb2f950e20d227673b6c6909a0
Chanhyuck Ko <lime_bell@naver.com>
2025-04-02 10:49:31 +0900

    chore: hide username temporary

--- src/components/MoveDisplay.tsx
+++ src/components/MoveDisplay.tsx
@@ -81,7 +81,6 @@ const MoveDisplay: React.FC<MoveDisplayProps> = ({ currentHp, gloveAddress, maxH
       {/* 플레이어 주소 공간 */}
       <div className="flex w-full items-center justify-center text-sm text-white p-1 bg-gray-800 rounded-b">
         {userAddress === 'you' ? t('ui:you') : <AddressDisplay address={userAddress} type='user' />}
-        {userAddress === 'you' ? '' : <div>({userName})</div>}
       </div>
     </div>
   );
