44c88b0e7d82c2bc0e61cc6049f66ea14459313d
Chanhyuck Ko <lime_bell@naver.com>
2025-04-17 20:12:39 +0900

    feat: display game rules

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,6 +6,9 @@ import gloveEN from './locales/en/glove.json';
 import gloveKO from './locales/ko/glove.json';
 import effectsEN from './locales/en/effects.json';
 import effectsKO from './locales/ko/effects.json';
+import roundRuleEN from './locales/en/roundRule.json';
+import roundRuleKO from './locales/ko/roundRule.json';
+
 
 i18n
   .use(initReactI18next)
@@ -14,12 +17,14 @@ i18n
       en: {
         ui: uiEN,
         glove: gloveEN,
-        effects: effectsEN
+        effects: effectsEN,
+        roundRule: roundRuleEN
       },
       ko: {
         ui: uiKO,
         glove: gloveKO,
-        effects: effectsKO
+        effects: effectsKO,
+        roundRule: roundRuleKO
       }
     },
     lng: navigator.language || "en",
